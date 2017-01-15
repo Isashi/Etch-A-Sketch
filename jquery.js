7ad156c0ae92615aa4c3542b538db58a7b4e20c0
Isashi <isashimitsui@gmail.com>
2017-01-15 21:46:13 +0100

    Modified jquery.js with Shade and Eraser

--- jquery.js
+++ jquery.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-GenGrid(45);
+GenGrid(20);
 Hover();
 
 function Hover(){
 	$('.unit').hover(function(){
 		$(this).css("background-color", "yellow");
-});
+	});
+}
+
+function HoverShade(){
+	$('.unit').hover(function(){
+		$(this).css("opacity", "+=0.1")
+		$(this).css("background-color", "yellow");
+	});
+}
+
+function Eraser(){
+	$('.unit').hover(function(){
+		$(this).css("background-color", "#DDDDDD");
+	});
 }
 
 function GenGrid(res){
@@ -13,22 +26,34 @@ function GenGrid(res){
 	for (var u = 0; u < (res*res); u++){
 			$("<div class='unit'></div>").appendTo('#container');	
 		}
-	varheight = (540/res)-2;
-	varwidth = (540/res)-2;
+	varheight = Math.floor((540/res)-2);
+	varwidth = Math.floor((540/res)-2);
 	$('.unit').css("height", varheight);
 	$('.unit').css('width', varwidth);
-	$(".unit").css("background-color","#111111");
-	Hover();
 }
 
 $('#clean').click(function(){
 	$(".unit").css("background-color","#DDDDDD");
 });
 
+$('#eraser').click(function(){
+	Eraser();
+});
+
+$('#shade').click(function(){
+	squares = +prompt('Square par side?');
+	if (squares >0){
+			GenGrid(squares);
+	};
+	$(".unit").css("opacity","0");
+	HoverShade();
+});
+
 $('#resize').on('click', function(){
 	squares = +prompt('Square par side?');
 	if (squares >0){
 			GenGrid(squares);
+			Hover()
 	};
 });
 

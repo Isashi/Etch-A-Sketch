77fc98fcd8a68b69580fab04ef08b50bc1e496b7
Isashi <isashimitsui@gmail.com>
2017-01-15 17:32:28 +0100

    Added Clean and Resize to jquery.js

--- jquery.js
+++ jquery.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
-		for (var u = 0; u < 2025; u++){
+GenGrid(45);
+Hover();
+
+function Hover(){
+	$('.unit').hover(function(){
+		$(this).css("background-color", "yellow");
+});
+}
+
+function GenGrid(res){
+	$('#container').empty();
+	for (var u = 0; u < (res*res); u++){
 			$("<div class='unit'></div>").appendTo('#container');	
 		}
+	varheight = (540/res)-2;
+	varwidth = (540/res)-2;
+	$('.unit').css("height", varheight);
+	$('.unit').css('width', varwidth);
+	$(".unit").css("background-color","#111111");
+	Hover();
+}
 
-
-$('.unit').hover(function(){
-	$(this).css("background-color", "yellow");
+$('#clean').click(function(){
+	$(".unit").css("background-color","#DDDDDD");
 });
+
+$('#resize').on('click', function(){
+	squares = +prompt('Square par side?');
+	if (squares >0){
+			GenGrid(squares);
+	};
+});
+
 });
